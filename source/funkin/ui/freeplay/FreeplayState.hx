f30489e73a8a04cbce470eaee64cebde62f7764e
gamerbross <55158797+gamerbross@users.noreply.github.com>
2024-06-09 00:29:55 +0200

    Fix Stack Overflow if song doesn't have "normal" difficulty

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -2058,6 +2058,8 @@ class FreeplaySongData
 
   function set_currentDifficulty(value:String):String
   {
+    if (currentDifficulty == value) return value;
+
     currentDifficulty = value;
     updateValues(displayedVariations);
     return value;
