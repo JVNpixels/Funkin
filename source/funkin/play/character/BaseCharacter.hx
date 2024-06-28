248c0917a2a3d3bbb435677fed0522a54baa3f9d
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2024-06-28 06:10:05 +0000

    we can probably just move this here

--- source/funkin/play/character/BaseCharacter.hx
+++ source/funkin/play/character/BaseCharacter.hx
@@ -272,6 +272,7 @@ class BaseCharacter extends Bopper
         {
           trace('Playing GF combo drop animation: ${dropAnim}');
           this.playAnimation(dropAnim, true, true);
+          comboAnimPlayed.clear();
         }
       case "combo":
         for (count in comboNoteCounts)
@@ -583,7 +584,6 @@ class BaseCharacter extends Bopper
     else if (event.note.noteData.getMustHitNote() && characterType == GF)
     {
       playCountAnimation("drop");
-      comboAnimPlayed.clear();
     }
   }
 
